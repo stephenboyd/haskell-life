f4068228828ab8d55b8fd5a74f38f13b674fccaa
Abrar Hussain <abrar.a.hussain@gmail.com>
2016-09-03 13:47:24 -0700

    Included basic animation.

--- life.hs
+++ life.hs
@@ -1,6 +1,7 @@
 import System.Random
 import Control.Monad
 import Control.Monad.Trans.State
+import Control.Concurrent (threadDelay)
 
 
 generateRandomMatrix :: Int -> Int -> StdGen -> [[Bool]]
@@ -15,15 +16,20 @@ generateRandomRow :: Int -> StdGen -> ([Bool], StdGen)
 generateRandomRow numCols = runState (replicateM numCols (state random))
 
 showMatrix :: [[Bool]] -> IO ()
-showMatrix matrix = putStr (concat (combineRows matrix))
+showMatrix matrix = putStr (flattenMatrix (convertMatrix matrix))
+
+flattenMatrix :: [[String]] -> String
+flattenMatrix [] = ""
+flattenMatrix (x:xs) = 
+    let combinedCols = (combineCols x)
+    in combinedCols ++ "\n" ++ (flattenMatrix xs)
+
+convertMatrix :: [[Bool]] -> [[String]]
+convertMatrix matrix = [ (convertRow row) | row <- matrix ]
 
 combineCols :: [String] -> String
 combineCols [] = ""
-combineCols (x:xs) = x ++ combineCols xs
-
-combineRows :: [[Bool]] -> [String]
-combineRows matrix = [ combinedRow ++ "\n" | row <- matrix, 
-        let combinedRow = (combineCols (convertRow row)) ]
+combineCols (x:xs) = x ++ (combineCols xs)
 
 convertRow :: [Bool] -> [String]
 convertRow row = [ (convertCell cell) | cell <- row ]
@@ -33,12 +39,35 @@ convertCell cell
     | cell  = "█"
     | otherwise = " "
 
+swapValue :: [Bool] -> Int -> [Bool]
+swapValue row ind = 
+    let (front,val:end) = splitAt ind row
+    in front ++ (not val):end
+
+-- TODO: remove as transition completed
+flipRow :: [Bool] -> [Bool]
+flipRow row = [ (not x) | x <- row ]
+        
+-- TODO: transition needs to update cells
+runTransition oldMatrix = [ (flipRow row) | row <- oldMatrix ] 
+         
+repeatLoop _ 0 = return ()
+repeatLoop matrix n =
+    do 
+        let nextMatrix = runTransition matrix
+        showMatrix matrix
+        putStrLn "\n"
+        threadDelay 500000
+        repeatLoop nextMatrix (n - 1)
+
 main :: IO ()
 main = do
-    let numRows = 60 
+    let numRows = 30 
     let numCols = 90 
     gen <- getStdGen
     let result = generateRandomMatrix numRows numCols gen
     putStrLn "\n"
     showMatrix result
+    let other = runTransition result
+    repeatLoop (result) 100
     putStrLn "\n"
